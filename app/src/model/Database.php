e3888e52ff4d5ddda2f59da1725b2c85aa4e0d91
Piioni <rangelc.juane@gmail.com>
2025-05-20 20:37:38 +0200

    Feat: Database config

---
+++ app/src/model/Database.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace model;
+use PDO;
+use PDOException;
+
+class Database
+{
+    private static $instance = null;
+    private PDO $pdo;
+
+    private function __construct()
+    {
+        $host = 'mysql';
+        $db = 'stock_hospitalario';
+        $user = 'user';
+        $pass = 'password';
+        $charset = 'utf8mb4';
+
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+
+        try {
+            $this->pdo = new PDO($dsn, $user, $pass, $options);
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+
+    public static function getInstance(): self
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function getPdo(): PDO
+    {
+        return $this->pdo;
+    }
+}
